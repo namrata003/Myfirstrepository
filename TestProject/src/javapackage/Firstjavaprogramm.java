5385fa855e5f6dd41911b024245ed57837d41c40
santo <santo@NAMSC007>
2025-09-16 00:18:53 +0530

    adding project to githun repo

---
+++ TestProject/src/javapackage/Firstjavaprogramm.java
@@ -0,0 +1,15 @@
+package javapackage;
+
+public class Firstjavaprogramm {
+
+	public static void main(String[] args) {
+	    chicken();
+    System.out.println("Hello Automation Engineer");
+    System.out.println(  "mummy");
+	}
+
+    public static void chicken () {
+    	System.out.println("Tummy");
+    	System.out.println("hello git");    }
+}
+   
